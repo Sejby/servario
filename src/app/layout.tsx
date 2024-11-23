7074c566a031425317a9b09bfc0f104f8c5da286
Honza <janschejbal03@gmail.com>
2024-11-24 00:18:43 +0100

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen`}>
         <Providers>
           <Toaster
             toastOptions={{
